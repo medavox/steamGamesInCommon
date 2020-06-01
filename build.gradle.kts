81cb61a513b6be21e651fac1ca3ff712f1f58d98
Adam Howard <medavox2+github@gmail.com>
2020-06-01 19:09:17 +0100

    chore: set up gradle build file
    right now, it's multiplatform supporting JVM and JS targets,
    and using the kotlinx.serialisation library and Ktor.
    also using the shadowJar plugin to generate fat jars,
    and ben-manes.versions to keep apprised of dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
-    kotlin("multiplatform") version "1.4-M1"
+    kotlin("multiplatform") version "1.3.70"
+    kotlin("plugin.serialization") version "1.3.70"
+    id ("org.jetbrains.dokka") version "0.10.1"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
+    id("com.github.ben-manes.versions") version "0.28.0"
 }
 
 group = "org.example"
@@ -14,17 +18,33 @@ kotlin {
     /* Targets configuration omitted. 
     *  To find out how to configure the targets, please follow the link:
     *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
-
+    js()  // JS target named 'js'
+    val ktor_version = "1.3.2"
     sourceSets {
         val commonMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-common"))
+                implementation("io.ktor:ktor-client-core:$ktor_version")
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
+            }
+        }
+
+        jvm().compilations["main"].defaultSourceSet {
+            dependencies {
+                implementation(kotlin("stdlib-jdk8"))
+                implementation("io.ktor:ktor-client-apache:$ktor_version")
+            }
+        }
+
+        js().compilations["main"].defaultSourceSet {
+            dependencies {
+                implementation(kotlin("stdlib-jdk8"))
+                implementation("io.ktor:ktor-client-js:$ktor_version")
             }
         }
     }
