ca2f687a1938dbd51ba7817ec1a943ee2ea7d46a
Adam Howard <medavox2+github@gmail.com>
2020-06-01 13:33:26 +0100

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("multiplatform") version "1.4-M1"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    mavenCentral()
+}
+
+kotlin {
+    /* Targets configuration omitted. 
+    *  To find out how to configure the targets, please follow the link:
+    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+    }
+}
---
+++ src/commonMain/kotlin/Main.kt
@@ -0,0 +1,13 @@
+/**Input: a list of steam vanityNames
+ * (https://steamcommunity.com/id/THIS_IS_YOUR_VANITY_NAME)
+ *
+ * Output: a list of games that the provided list of users all own
+ * TODO: include free games that:
+ *   1) they have all played before
+ *   2) at minimum number have played before
+ *   3) a minimum percentage of the group has played before
+ *   4) the average playtime is above a certain amount (total group playtime divided by number of players)
+ *   5) at least one player has played before*/
+fun entryPoint(key:String, vararg players:String):List<String> {
+    return listOf()
+}
