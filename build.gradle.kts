bdfa1d152db6014aaea7d64369c8c027be562d77
Adam Howard <medavox2+github@gmail.com>
2020-06-26 22:07:42 +0100

    chore: fix gradle build file to allow shadowJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    kotlin("multiplatform") version "1.3.70"
-    kotlin("plugin.serialization") version "1.3.70"
+    kotlin("multiplatform") version "1.3.72"
+    kotlin("plugin.serialization") version "1.3.72"
+    id ("maven-publish")
     id ("org.jetbrains.dokka") version "0.10.1"
     id("com.github.johnrengelman.shadow") version "5.2.0"
     id("com.github.ben-manes.versions") version "0.28.0"
@@ -10,16 +11,27 @@ group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    //maven("https://dl.bintray.com/kotlin/kotlin-eap")
     mavenCentral()
     jcenter()
 }
 
 kotlin {
-    /* Targets configuration omitted. 
+    /* Targets configuration omitted.
     *  To find out how to configure the targets, please follow the link:
     *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
-    js()  // JS target named 'js'
+    jvm {
+        withJava()
+        val main by compilations.getting {
+            kotlinOptions {
+                // Setup the Kotlin compiler options for the 'main' compilation:
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    js{
+        browser()
+    }  // JS target named 'js'
     val ktor_version = "1.3.2"
     val serialization_version = "0.20.0"
     sourceSets {
@@ -37,16 +49,15 @@ kotlin {
             }
         }
 
-        jvm().compilations["main"].defaultSourceSet {
+        val jvmMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-jdk8"))
                 implementation("io.ktor:ktor-client-apache:$ktor_version")
                 implementation ("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$serialization_version")
-
             }
         }
 
-        js().compilations["main"].defaultSourceSet {
+        val jsMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-jdk8"))
                 implementation("io.ktor:ktor-client-js:$ktor_version")
@@ -54,5 +65,34 @@ kotlin {
 
             }
         }
+    }
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+
+val run by tasks.creating(JavaExec::class) {
+    group = "application"
+    main = "com.jetbrains.handson.introMpp.MainKt"
+    kotlin {
+        val main = targets["jvm"].compilations["main"]
+        dependsOn(main.compileAllTaskName)
+        classpath(
+            { main.output.allOutputs.files },
+            { configurations["jvmRuntimeClasspath"] }
+        )
+    }
+    ///disable app icon on macOS
+    systemProperty("java.awt.headless", "true")
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    minimize()
+    manifest {
+        attributes["Implementation-Version"] = version
+        attributes["Main-Class"] = "JvmMainKt"
     }
 }
