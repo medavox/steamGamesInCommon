d11d1a7982444e349d30e4de099c4574b0bfeecb
Adam Howard <medavox2+github@gmail.com>
2020-06-27 20:37:28 +0100

    add name cache builder, the cache itself
    also the JvmMain file, which was oddly missing from git

---
+++ src/jvmMain/kotlin/NameCacheBuilder.kt
@@ -0,0 +1,153 @@
+import io.ktor.client.HttpClient
+import io.ktor.client.features.HttpTimeout
+import io.ktor.client.request.get
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import kotlinx.serialization.json.JsonObject
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.util.*
+
+/**Downloads names for app IDs using several different HTTP routes,
+ * to build a local game_name_cache.properties file that can be packaged with all versions of this app,
+ * saving having to download again on every instance
+ * (speeds up the app, and reduces strain on our API Key's usage limits)*/
+suspend fun doit(key: String, vararg players: String) = coroutineScope {
+    val client = HttpClient()
+    // request all player IDs asynchronously in parallel.
+    //get 64-bit steam ID from 'vanityName' (mine is addham):
+    val playerSchemas = players.map {
+        async {
+            println("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=$key&vanityurl=$it")
+            client.get<String>(
+                    "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=$key&vanityurl=$it"
+            )
+        }
+    }
+
+    // Get the request contents without blocking threads, but wait until all requests are done.
+    // Suspension point.
+    val json = Json(JsonConfiguration.Stable)
+    //.await() BOTH waits for the result to complete AND gets its result
+    val playerIDs: List<String> = playerSchemas.map {
+        //println(it.await())
+        json.parseJson(it.await()).jsonObject["response"]?.jsonObject
+    }.map { it?.get("steamid")?.toString()?.trim{ it == '"'} }.filterNotNull()
+
+    players.forEachIndexed { i, s -> println("$s: ${playerIDs[i]}") }
+
+    //get list of owned games for provided list of 64-bit steam IDs (comma-seperated) (profiles must be public):
+    //http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$key&steamid=76561197979296883&format=json
+    //the Pair<String, String> is app id: play time forever
+    val ownedGames:Map<String, List<Pair<String, String>>?> = playerIDs.associate { id: String ->
+        val games = async {
+            client.get<String>(
+                    "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$key&steamid=$id&format=json"
+            )
+        }
+        val gamesJson:JsonObject? = json.parseJson(games.await()).jsonObject["response"]?.jsonObject
+        val gameIdList = gamesJson?.get("games")?.jsonArray
+        val idPlayTimePairs:List<Pair<String, String>>? = gameIdList?.map {
+            val asObj = it.jsonObject
+            val appid = asObj["appid"]?.primitive?.contentOrNull
+            val playtime = asObj["playtime_forever"]?.primitive?.contentOrNull
+            if(appid != null && playtime != null) Pair(appid, playtime) else null
+        }?.filterNotNull()
+        //println(gameIdList)
+        Pair(id, idPlayTimePairs)
+    }
+
+    //create a Set<String> of unique app IDs, so we only have to lookup the name of each game once
+    val gameIds:MutableSet<String> = mutableSetOf()
+    ownedGames.values.filterNotNull().forEach{ playerGameList ->
+        playerGameList.forEach { (appid, playtime) ->
+            gameIds.add(appid)
+        }
+    }
+
+    //todo: defer id-to-name lookups until the last step, to reduce the amount of querying of steam's shitty REST routes
+
+    //convert each app ID to its game name
+    //=====================================
+
+    //http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=$key&appid=218620
+    val gameIdsToNames:MutableMap<String, String> = mutableMapOf()
+
+    //http://store.steampowered.com/api/appdetails/?appids=
+    val missingDataIds:MutableSet<String> = mutableSetOf()
+    val cacheFile = File("game_name_cache.properties")
+    if(!cacheFile.exists()) cacheFile.createNewFile()
+    val cachedNames = Properties()
+    val inputStream: InputStream = FileInputStream(cacheFile)
+    cachedNames.load(inputStream)
+    inputStream.close()
+
+    //initialise cache from local file
+    //val cachedIdNameEntries:Map<String, String> =//todo: figure out how to load local resource files on common?
+    println("${gameIds.size} game IDs to lookup")
+    gameIds.forEach { appid ->
+        val highTimeoutClient = HttpClient() {
+            install(HttpTimeout) {
+                // timeout config
+                requestTimeoutMillis = 30_000
+            }
+        }
+        if(cachedNames.contains(appid)) {
+            gameIdsToNames.put(appid, cachedNames.getProperty(appid))
+        }else {
+            //println("http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=$key&appid=$appid")
+            val nameOfIdDeferred = async {
+                highTimeoutClient.get<String>(
+                        "http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=$key&appid=$appid"
+                )
+            }
+            //despite the name 'appids', the store API no longer supports multiple appids, for some unknown reason:
+            //https://www.reddit.com/r/Steam/comments/2kz2ay/steam_store_api_multiple_app_id_lists_no_longer/
+            val schemaDeferred = nameOfIdDeferred.await()
+            //println("rawdata length: ${schemaDeferred.length}")
+            val gameSchema: JsonObject? = json.parseJson(schemaDeferred).jsonObject["game"]?.jsonObject
+            var gameName: String? = null
+            if (gameSchema == null || !gameSchema.containsKey("gameName")) {
+                //problem: sometimes, the game data retrieved by this route is empty
+                //so we have to perform some workarounds, as described in the above reddit post
+
+
+                //1. check the local cachefile (CSV) for game name
+                //if that fails, add it to the backupApi query list
+                //http://store.steampowered.com/api/appdetails/?appids=<only one appid>
+                //the above query blocks you with status 429 if you hit it roughly more than 200 times in 5 minutes, apparently
+                //option C:
+                //https://store.steampowered.com/app/<appid>
+                //find the human-readable name in the raw HTML of the store page:
+                // <div class="apphub_AppName">{human-readable name}</div>
+                missingDataIds.add(appid)
+            } else {//the game data exists, so get the name from it
+                //ANOTHER PROBLEM: sometimes, the name is replaced with "ValveTestApp<AppId>"
+                //so for AppId 72850, it's ValveTestApp72850
+                val possibleName = gameSchema["gameName"].toString().trim { it == '"' }
+                if (!possibleName.startsWith("ValveTestApp") && !possibleName.contains("UntitledApp") && !possibleName.isBlank() && !possibleName.isEmpty()) {//it actually is fine
+                    cachedNames.put(appid, possibleName)
+                    gameIdsToNames.put(appid, possibleName)
+                    println("$appid: ${possibleName ?: "<null>"}")
+                } else {
+                    //bollocks
+                    missingDataIds.add(appid)
+                }
+            }
+        }
+    }
+    //write out any newly retrieved game names to the .properties cachefile
+    val fos = FileOutputStream(cacheFile)
+    cachedNames.store(fos, "")
+    fos.close()
+
+    missingDataIds.forEach {
+
+    }
+    println("${missingDataIds.size} or ${(missingDataIds.size*1000) / gameIds.size }‰ of name lookups failed")
+    client.close()
+}
