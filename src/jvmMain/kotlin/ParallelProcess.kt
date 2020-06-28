299e86711caa2b76f30a2a61a8b48c2479f8ec08
Adam Howard <medavox2+github@gmail.com>
2020-06-28 14:14:50 +0100

    ParallelProcess: update collectOutputWhenFinished to not need InternalWorker either

--- src/jvmMain/kotlin/ParallelProcess.kt
+++ src/jvmMain/kotlin/ParallelProcess.kt
@@ -33,10 +33,8 @@ internal class ParallelProcess<In, Out>(private val workFunction: (input: In) ->
     /**Get the resulting output from the worker threads.
      * This is a blocking method; it only returns once all worker threads have finished.*/
     fun collectOutputWhenFinished(): List<Out> {
-        val outputInProgress = mutableSetOf<Out?>()//nullable because the worker may fail to produce output
         for(thread in workerThreads) {
             thread.join()
-            outputInProgress.add(thread.getOutput())
         }
         //get rid of null elements, convert from MutableSet<Out?> to List<Out>
         return outputInProgress.filterNotNull().toList()
