72bca0d9716e758a1601ed7df840c1293e294ee7
Adam Howard <medavox2+github@gmail.com>
2020-06-28 16:40:03 +0100

    rewrite NameCacheBuilder to use okHttp and ParallelProcesses

--- src/jvmMain/kotlin/NameCacheBuilder.kt
+++ src/jvmMain/kotlin/NameCacheBuilder.kt
@@ -1,13 +1,8 @@
-import io.ktor.client.HttpClient
-import io.ktor.client.features.HttpTimeout
-import io.ktor.client.features.RedirectResponseException
-import io.ktor.client.request.get
-import kotlinx.coroutines.async
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.coroutineScope
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
 import kotlinx.serialization.json.JsonObject
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
@@ -18,57 +13,66 @@ import java.util.*
  * to build a local game_name_cache.properties file that can be packaged with all versions of this app,
  * saving having to download again on every instance
  * (speeds up the app, and reduces strain on our API Key's usage limits)*/
-suspend fun buildNameCache(key: String, vararg players: String) = coroutineScope {
-    val client = HttpClient()
+fun buildNameCache(key: String, vararg players: String) {
+    val NUM_THREADS = 8
+    val client = OkHttpClient()
     val json = Json(JsonConfiguration.Stable)
+    //STEP 1
+    //=====================================
     // request all player IDs asynchronously in parallel.
     //get 64-bit steam ID from 'vanityName' (mine is addham):
-    val playerIDs:List<String> = players.map {
-        if(it.matches(Regex("\\d{17}"))) {//is already a SteamId
-            it
+    val pp1 = ParallelProcess<String, String?>()
+    pp1.processMutableQueueWithWorkerPool(LinkedList(players.toList()), { vanityOrHash ->
+        if(vanityOrHash.matches(Regex("\\d{17}"))) {//is already a SteamId
+            vanityOrHash
         }else {
-            val response = async {
-                println("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=$key&vanityurl=$it")
-                client.get<String>(
-                        "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=$key&vanityurl=$it"
-                )
-            }.await()
-            json.parseJson(response).jsonObject["response"]?.jsonObject?.get("steamid")?.toString()?.trim{ c -> c == '"'}
+            val url = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=$key&vanityurl=$vanityOrHash"
+            println(url)
+            val request:Request = Request.Builder()
+                    .url(url)
+                    .build()
+            val response:String? = client.newCall(request).execute().body?.string()
+            if(response == null) println("ERROR: got null response for ID $vanityOrHash")
+            response?.let{
+                json.parseJson(response).jsonObject["response"]?.
+                    jsonObject?.get("steamid")?.toString()?.trim{ c -> c == '"'}
+            }
         }
-    }.filterNotNull()
+    }, NUM_THREADS)
+    val playerIDs:List<String> = pp1.collectOutputWhenFinished().filterNotNull()
 
-    // Get the request contents without blocking threads, but wait until all requests are done.
-    // Suspension point.
-
-/*    val playerIDs: List<String> = playerSchemas.map {
-        json.parseJson(it).jsonObject["response"]?.jsonObject
-    }.map { it?.get("steamid")?.toString()?.trim{ c -> c == '"'} }.filterNotNull()*/
-
-    //players.forEachIndexed { i, s -> println("$s: ${playerIDs[i]}") }
-
+    //STEP 2
+    //=====================================
     //get list of owned games for each 64-bit steam ID (comma-separated) (profiles must be public):
     //http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$key&steamid=76561197979296883&format=json
-    //the Pair<String, String> is app id: play time forever
-    val ownedGames:Map<String, List<Pair<String, String>>?> = playerIDs.associate { id: String ->
-        val games = async {
-            println("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$key&steamid=$id&format=json")
-            client.get<String>(
-                    "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$key&steamid=$id&format=json"
-            )
+    //Pair<appid:String, playtime:String>
+    val pp2 = ParallelProcess<String, Pair<String, List<Pair<String, String>>?>>()
+    pp2.processMutableQueueWithWorkerPool(LinkedList(playerIDs), { id: String ->
+        val url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$key&steamid=$id&format=json"
+        println(url)
+        val request:Request = Request.Builder().url(url).build()
+        val response:String? = client.newCall(request).execute().body?.string()
+        if(response == null) {
+            println("ERROR: got null response for game library request for ID $id")
+            //Pair(id, listOf<Pair<String, String>>())
+            null
+        }else {
+            val gamesJson: JsonObject? = json.parseJson(response).jsonObject["response"]?.jsonObject
+            val gameIdList = gamesJson?.get("games")?.jsonArray
+            val idPlayTimePairs: List<Pair<String, String>>? = gameIdList?.mapNotNull {
+                val asObj = it.jsonObject
+                val appid = asObj["appid"]?.primitive?.contentOrNull
+                val playtime = asObj["playtime_forever"]?.primitive?.contentOrNull
+                if (appid != null && playtime != null) Pair(appid, playtime) else null
+            }
+            if (idPlayTimePairs?.isEmpty() != false) {
+                println("got zero games for steam ID $id; is the profile public?")
+            }
+            Pair(id, idPlayTimePairs)
         }
-        val gamesJson:JsonObject? = json.parseJson(games.await()).jsonObject["response"]?.jsonObject
-        val gameIdList = gamesJson?.get("games")?.jsonArray
-        val idPlayTimePairs:List<Pair<String, String>>? = gameIdList?.map {
-            val asObj = it.jsonObject
-            val appid = asObj["appid"]?.primitive?.contentOrNull
-            val playtime = asObj["playtime_forever"]?.primitive?.contentOrNull
-            if(appid != null && playtime != null) Pair(appid, playtime) else null
-        }?.filterNotNull()
-        if(idPlayTimePairs?.isEmpty() != false) {
-            println("got zero games for steam ID $id; is the profile public?")
-        }
-        Pair(id, idPlayTimePairs)
-    }
+    }, NUM_THREADS)
+
+    val ownedGames:Map<String, List<Pair<String, String>>?> = pp2.collectOutputWhenFinished().toMap()
 
     //create a Set<String> of unique app IDs, so we only have to lookup the name of each game once
     val gameIds:MutableSet<String> = mutableSetOf()
@@ -78,16 +82,14 @@ suspend fun buildNameCache(key: String, vararg players: String) = coroutineScope
         }
     }
 
-    //todo: defer id-to-name lookups until the last step, to reduce the amount of querying of steam's shitty REST routes
-
-    //convert each app ID to its game name
+    //STEP 3
     //=====================================
+    //convert each app ID to its game name
 
     //http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=$key&appid=218620
     val gameIdsToNames:MutableMap<String, String> = mutableMapOf()
 
     //http://store.steampowered.com/api/appdetails/?appids=
-    val missingDataIds:MutableSet<String> = mutableSetOf()
     val cacheFile = File("game_name_cache.properties")
     if(!cacheFile.exists()) cacheFile.createNewFile()
     val cachedNames = Properties()
@@ -101,34 +103,41 @@ suspend fun buildNameCache(key: String, vararg players: String) = coroutineScope
     //val cachedIdNameEntries:Map<String, String> =//todo: figure out how to load local resource files on common?
     println("${gameIds.size} game IDs to lookup")
     try {
-        if(missingDataIds.isNotEmpty()) println("scraping ${missingDataIds.size} missing entries from HTML of store pages...")
-        for(appid in gameIds) {
-            if (cachedNames.getProperty(appid) != null) {
-                continue
-            }
-            val storePageHtml = async {
-                try {
-                    client.get<String>("https://store.steampowered.com/app/$appid")
-                }catch(rre: RedirectResponseException) {
-                    rre.printStackTrace()
-                    println("failed to get name of app $appid from store page scraper: ${rre::class.java}")
-                    ""
-                }
-            }.await()
-            if(storePageHtml.isEmpty()) continue
-
-            val nameInTitle = Regex("<title>(?:Save \\d{1,3}% on )?(.+) on Steam</title>")
-            //find the human-readable name in the raw HTML of the store page:
-            // <div class="apphub_AppName">{human-readable name}</div>
-            val nameFromTitleResult: MatchResult? = nameInTitle.find(storePageHtml)
-            val possibleName: String? = nameFromTitleResult?.groupValues?.get(1)
-            possibleName?.let {
-                println("name found on store page: $possibleName")
-                cachedNames.setProperty(appid, possibleName)
-                gameIdsToNames.put(appid, possibleName)
-                missingDataIds.remove(appid)
-            }
+        //filter out any game IDs that we already have in the cache
+        val gameIdsToLookup = gameIds.filter { appid ->
+            cachedNames.getProperty(appid) == null
         }
+        pp1.processMutableQueueWithWorkerPool(LinkedList(gameIdsToLookup), { appid ->
+            val url = "https://store.steampowered.com/app/$appid"
+            val request:Request = Request.Builder().url(url)
+                    //thanks to https://stackoverflow.com/q/17643851
+                    .header("Range", "bytes=0-511")//only download the beginning of the storepage HTML
+                    //we just need the contents of the <title> tag, and we're not parsing it as valid html anyway
+                    .build()
+            val response:String? = client.newCall(request).execute().body?.string()
+            println("response:")
+            println("$response")
+            if(response.isNullOrEmpty()) {
+                null//exit early with a null result
+            }else {
+                val nameInTitle = Regex("<title>(?:Save \\d{1,3}% on )?(.+) on Steam</title>")
+                //find the human-readable name in the raw HTML of the store page:
+                // <div class="apphub_AppName">{human-readable name}</div>
+                val nameFromTitleResult: MatchResult? = nameInTitle.find(response)
+                val possibleName: String? = nameFromTitleResult?.groupValues?.get(1)
+                possibleName?.let {
+                    println("name found on store page: $possibleName")
+                    cachedNames.setProperty(appid, possibleName)
+                    gameIdsToNames.put(appid, possibleName)
+                }
+                possibleName
+            }
+        }, NUM_THREADS)
+
+        val results = pp1.collectOutputWhenFinished()//this output collection isn't the most important data storage;
+        // the real work is in the Properties file, which now needs writing to disk
+        val failures = results.count { it == null }
+        println("$failures or ${(failures*1000) / gameIds.size }‰ of name lookups failed")
     }finally {
         //write out any newly retrieved game names to the .properties cachefile
         val fos = FileOutputStream(cacheFile)
@@ -136,7 +145,5 @@ suspend fun buildNameCache(key: String, vararg players: String) = coroutineScope
         fos.close()
     }
 
-    println("${missingDataIds.size} or ${(missingDataIds.size*1000) / gameIds.size }‰ of name lookups failed:")
-    println(missingDataIds)
-    client.close()
+
 }
