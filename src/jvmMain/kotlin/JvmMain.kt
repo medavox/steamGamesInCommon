d2068de220560014f8738f756238d082de6be464
Adam Howard <medavox2+github@gmail.com>
2020-06-30 12:54:00 +0100

    JvmMain: remove dead code
    old commented-out Clikt stuff

--- src/jvmMain/kotlin/JvmMain.kt
+++ src/jvmMain/kotlin/JvmMain.kt
@@ -2,16 +2,10 @@ import kotlinx.coroutines.runBlocking
 import java.util.*
 
 fun main(args:Array<String>) = runBlocking<Unit> {
-    //ArgParser().main(args)
     if(args.size < 2) {
         System.err.println("required arguments: <steam web API key> [player]...")
     }
     val names = Arrays.copyOfRange(args, 1, args.size)
     //buildNameCache(args[0], *names)
     steamGamesInCommon(args[0], *names)
-/*    if(args.size >= 2) {
-        entryPoint(args[0], *(args.slice(1 until args.size).toTypedArray()))
-    }else {
-        println("use it properly, dummy!")
-    }*/
 }
