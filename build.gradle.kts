c81878f8f35a38a05681477c5ff59e52e07607d6
Adam Howard <medavox2+github@gmail.com>
2020-07-03 15:14:45 +0100

    convert to JVM-only Kotlin project
    every environment we'll be using going forwards is JVM-only,
    which affords us a wider choice of libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.3.72"
+    java
+    kotlin("jvm") version "1.3.72"
     kotlin("plugin.serialization") version "1.3.72"
     id ("maven-publish")
     id ("org.jetbrains.dokka") version "0.10.1"
@@ -15,73 +16,16 @@ repositories {
     mavenCentral()
     jcenter()
 }
-
-kotlin {
-    /* Targets configuration omitted.
-    *  To find out how to configure the targets, please follow the link:
-    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
-    jvm {
-        withJava()
-        val main by compilations.getting {
-            kotlinOptions {
-                // Setup the Kotlin compiler options for the 'main' compilation:
-                jvmTarget = "1.8"
-            }
-        }
-    }
-    js{
-        browser {
-            dceTask {
-                keep("ktor-ktor-io.\$\$importsForInline\$\$.ktor-ktor-io.io.ktor.utils.io")
-            }
-            distribution {
-                directory = File("$projectDir/output/")
-            }
-        }
-    }  // JS target named 'js'
-    val ktor_version = "1.3.2"
-    val serialization_version = "0.20.0"
-    val coroutines_version = "1.3.7"
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-common"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:$coroutines_version")
-                implementation("io.ktor:ktor-client-core:$ktor_version")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-common:$serialization_version")
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
-
-        val jvmMain by getting {
-            dependencies {
-                implementation("com.squareup.okhttp3:okhttp:4.7.2")
-                implementation(kotlin("stdlib-jdk8"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
-                implementation("io.ktor:ktor-client-apache:$ktor_version")
-                implementation ("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$serialization_version")
-            }
-        }
-
-        val jsMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-jdk8"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:$coroutines_version")
-                implementation("io.ktor:ktor-client-js:$ktor_version")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-js:$serialization_version")
-                api(npm("text-encoding"))
-                api(npm("bufferutil"))
-                api(npm("utf-8-validate"))
-                api(npm("abort-controller"))
-                api(npm("fs"))
-            }
-        }
-    }
+val ktor_version = "1.3.2"
+val serialization_version = "0.20.0"
+val coroutines_version = "1.3.7"
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("com.squareup.okhttp3:okhttp:4.7.2")
+    implementation("org.jsoup:jsoup:1.13.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
+    implementation("io.ktor:ktor-client-apache:$ktor_version")
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$serialization_version")
 }
 
 java {
@@ -90,7 +34,7 @@ java {
 }
 
 
-val run by tasks.creating(JavaExec::class) {
+/*val run by tasks.creating(JavaExec::class) {
     group = "application"
     main = "com.jetbrains.handson.introMpp.MainKt"
     kotlin {
@@ -103,7 +47,7 @@ val run by tasks.creating(JavaExec::class) {
     }
     ///disable app icon on macOS
     systemProperty("java.awt.headless", "true")
-}
+}*/
 
 tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
     minimize()
--- src/jsMain/kotlin/UiStrings.kt
+++
@@ -1,5 +0,0 @@
-object UiStrings {
-    val errorsHint = "errors appear here"
-    val outputHint = "transliteration appears here"
-    val inputHint = "enter native text"
-}
