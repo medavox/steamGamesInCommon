e7433514b2ef49844f8db99f9fb927aa3daf5db5
Adam Howard <medavox2+github@gmail.com>
2020-07-04 16:46:55 +0100

    remove dead code from RedisDataInit

--- src/main/kotlin/RedisDataInit.kt
+++ src/main/kotlin/RedisDataInit.kt
@@ -1,18 +1,5 @@
-
-import kotlinx.serialization.json.*
-import org.jsoup.parser.Parser
-import redis.clients.jedis.Jedis
-
-import redis.clients.jedis.params.SetParams
-import java.io.File
-import java.lang.NullPointerException
-
-object RedisDataInit {
-    val e = System.err
-
-}
-
 fun main() {
-    RedisDataInit.convertKotlinMap(gameNameCache)
-    RedisDataInit.convertJsonAppList("appList.json")
+    val r = LocalRedisApi()
+    r.convertKotlinMap(gameNameCache)
+    r.convertJsonAppList("appList.json")
 }
